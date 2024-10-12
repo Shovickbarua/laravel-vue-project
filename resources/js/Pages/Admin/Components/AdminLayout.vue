33d72a68474508120dbd964d409034c6fdf12ea0
shovickbarua <shovickbarua92@gmail.com>
2024-10-12 16:16:49 +0600

    update

---
+++ resources/js/Pages/Admin/Components/AdminLayout.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { onMounted } from 'vue'
+import { initFlowbite } from 'flowbite'
+import Footer from './Footer.vue';
+import Navbar from './Navbar.vue';
+import Sidebar from './Sidebar.vue';
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+    initFlowbite();
+})
+</script>
+
+<template>
+    <div class="antialiased bg-gray-50 dark:bg-gray-900">
+    <!-- Navbar -->
+        <Navbar/>
+    <!-- Sidebar -->
+        <Sidebar/>
+
+        <slot/>
+
+    <!-- Footer -->
+        <Footer/>
+    </div>
+</template>
